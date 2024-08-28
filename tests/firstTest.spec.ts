1e3396c32dc1f8733acfb4a38510ee62e9399ccc
iamSymbiote <burakguvenc@outlook.com>
2024-08-28 20:06:57 +0300

    form layout button changed and getby some new stuff

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,31 +1,52 @@
-import {test} from '@playwright/test';
+import {test, expect} from '@playwright/test'
 
-test.beforeEach(async ({ page }) => {
+
+test.beforeEach(async({page}) => {
     await page.goto('localhost:4200')
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
 })
 
-test.describe.skip('suite1', () => {
-    test.beforeEach(async ({ page }) => {
-        await page.getByText('Charts').click()
-    })
-    test('the first test', async ({ page }) => {
-        await page.getByText('Form Layouts').click()
-    })
+test('Locator syntax rules', async({page}) => {
+    //by Tag name
+    await page.locator('input').first().click()
 
-    test('date picker check', async ({ page }) => {
-        await page.getByText('Date Picker').click()
-    })
+    //by ID
+    page.locator('#inputEmail1')
+
+    //by Class value
+    page.locator('.shape-rectangle')
+
+    //by attribute
+    page.locator('[placeholder="Email"]')
+
+    //by Class value (full)
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+    //combine different selectors
+    page.locator('input[placeholder="Email"][nbinput]')
+
+    // by XPath (NOT RECOMMENDED)
+    page.locator('//*[@id="inputEmail1"]')
+
+    // by partial text match
+    page.locator(':text("Using")')
+
+    //by exact text match
+    page.locator(':text-is("Using the Grid")')
 })
 
-test.describe('suite1', () => {
-    test.beforeEach(async ({ page }) => {
-        await page.getByText('Forms').click()
-    })
-    test('the first test', async ({ page }) => {
-        await page.getByText('Form Layouts').click()
-    })
+test('User facing locators', async({page}) => {
+    await page.getByRole('textbox', {name: "Email"}).first().click()
+    await page.getByRole('button', {name: "Sign in"}).first().click()
 
-    test('date picker check', async ({ page }) => {
-        await page.getByText('Date Picker').click()
-    })
+    await page.getByLabel('Email').first().click()
+
+    await page.getByPlaceholder('Jane Doe').click()
+
+    await page.getByText('Using the Grid').click()
+
+    await page.getByTestId('SignIn').click()
+
+    await page.getByTitle('IoT Dashboard').click()
 })
